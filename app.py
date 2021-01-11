7050aefdd1710f68384240c102d86040ee100767
Hasmot Ali Hasu <hasmot23@gmail.com>
2021-01-12 00:23:13 +0600

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ clf = pickle.load(open(filename, 'rb'))
 cv=pickle.load(open('tranform.pkl','rb'))
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
 	return render_template('home.html')
+
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -19,6 +21,7 @@ def predict():
 		vect = cv.transform(data).toarray()
 		my_prediction = clf.predict(vect)
 	return render_template('result.html',prediction = my_prediction)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
